088a77e291c6b30566822575a22007f7436030ac
Joost Holslag <info@joostholslag.nl>
2016-01-28 21:14:03 +0100

    implemented connections and methods

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["From what is cagnac made?", "what is 7_7?", "What is the capital of Vermont?"
+    ]
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+    ++currentQuestionIndex
+        if currentQuestionIndex == questions.count{
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
     }
-
-
-}
+    
+    
+ }
 
