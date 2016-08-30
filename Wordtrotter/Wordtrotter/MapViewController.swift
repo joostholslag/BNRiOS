7a22214f1c5e48581af29da26263fa049cee12dc
Joost Holslag <info@joostholslag.nl>
2016-08-30 16:09:23 +0200

    working

--- Wordtrotter/Wordtrotter/MapViewController.swift
+++ Wordtrotter/Wordtrotter/MapViewController.swift
@@ -19,7 +19,11 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         locationManager = CLLocationManager()
         // Set it as *the* view of this view controller
         view = mapView
+        addUIElements()
         
+    }
+    
+    func addUIElements(){
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
         segmentedControl.selectedSegmentIndex = 0
@@ -42,6 +46,28 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         topConstraint.active = true
         leadingConstraint.active = true
         trailingConstraint.active = true
+        
+        let showLocButton = UIButton(type: .System)
+        showLocButton.setTitle("Show Loc", forState: .Normal)
+        showLocButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(showLocButton)
+        showLocButton.addTarget(self, action: #selector(MapViewController.showLocButton(_:)), forControlEvents: .TouchUpInside)
+        
+        let topButtonConstraint = showLocButton.topAnchor.constraintEqualToAnchor(segmentedControl.bottomAnchor, constant: 8)
+        let leadingButtonConstraint = showLocButton.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
+        let trailingButtonConstraint = showLocButton.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
+        topButtonConstraint.active = true
+        leadingButtonConstraint.active = true
+        trailingButtonConstraint.active = true
+        
+    }
+    func showLocButton(sender: UIButton!) {
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+    }
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        let zoomedInCurrentLocation = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 500, 500)
+        mapView.setRegion(zoomedInCurrentLocation, animated: true)
     }
     
     func mapTypeChanged(segControl: UISegmentedControl) {
